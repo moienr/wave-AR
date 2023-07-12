4cb7a6e0c27c2b8a9b82bde1bf32679eb8a896bb
moi3n <moi3nr@gmail.com>
2023-07-12 23:47:28 +0330

    added project code

---
+++ proj/Assets/Scripts/X.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class X : MonoBehaviour
+{
+    public float x;
+    
+    public float xAmplitude = 10;
+    public teta tet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tet = GameObject.Find("Teta").GetComponent<teta>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = xAmplitude* Mathf.Cos(tet.Teta * Mathf.PI / 180);
+        transform.position = new Vector3( x, transform.position.y , transform.position.z);
+    }
+}
---
+++ proj/Assets/Scripts/teta.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teta : MonoBehaviour
+{
+    public float Teta;
+    public float Speed =1;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Teta = Mathf.PingPong(Time.time * Speed  , 36000 ) ;
+       
+         Debug.Log("teta:  " + Teta);
+    }
+}
