8c3f47cbac5e61498c386095a5a27b32f58af95e
moi3n <moi3nr@gmail.com>
2023-07-12 23:52:09 +0330

    added proj code

---
+++ proj/Assets/Scripts/Y.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Y : MonoBehaviour
+{
+      public float y;
+      public float phaseDifrens = -90;  
+    public float yAmplitude = 10;
+    public teta tet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tet = GameObject.Find("Teta").GetComponent<teta>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            y= yAmplitude* Mathf.Cos((tet.Teta + phaseDifrens) * Mathf.PI / 180);
+        transform.position = new Vector3( transform.position.x , y , transform.position.z);
+    }
+}
---
+++ proj/Assets/Scripts/wave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wave : MonoBehaviour
+{
+    public X xScript;
+    public Y yScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xScript = GameObject.Find("X").GetComponent<X>();
+        yScript = GameObject.Find("Y").GetComponent<Y>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 ( xScript.x , yScript.y , transform.position.z);
+    }
+}
